88ad05eb42d950c12ee25ca180688d68856c2319
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2024-01-17 10:31:32 -0500

    put delete post and edit implemented

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,11 +1,56 @@
 const express = require('express');
-const route = require.Router();
+const router = express.Router();
 const Recipe = require('../models/recipe');
 
-//route to display all recipes
-
-route
-.get('/',async(req,res)=>{
+// Route to display all recipes
+router.get('/', async (req, res) => {
+  try {
     const recipes = await Recipe.find({});
-    res.render('recipes/index',{recipes})
-})
+    res.render('recipes/index', { recipes });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Route to create a new recipe (POST)
+router.post('/', async (req, res) => {
+  try {
+    const newRecipe = new Recipe(req.body);
+    await newRecipe.save();
+    res.redirect('/recipes');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Route to fetch a form for editing a recipe (GET)
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    res.render('recipes/edit', { recipe });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Route to update a recipe (PUT)
+router.put('/:id', async (req, res) => {
+  try {
+    await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.redirect('/recipes/' + req.params.id);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Route to delete a recipe (DELETE)
+router.delete('/:id', async (req, res) => {
+  try {
+    await Recipe.findByIdAndDelete(req.params.id);
+    res.redirect('/recipes');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
